450929bad95689ed30a6a00f2febb3d7d4b5908c
web7302 <web7302@caelum57-06>
2018-04-21 17:40:25 -0300

    Spring IOC 21/04

--- fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -2,6 +2,7 @@ package br.com.caelum.tarefas.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,11 +15,23 @@ import br.com.caelum.tarefas.modelo.Tarefa;
 @Controller
 public class TarefasController {
 
-//	@RequestMapping("novaTarefa")
-//	public String form() {
-//		return "tarefa/formulario";
-//	}
+	@RequestMapping("novaTarefa")
+	public String form() {
+		return "tarefa/formulario";
+	}
 
+//	@Autowired - Erro qu•••••e aconteceu ao definir a anatacao para o atributo ao inves de ser no construtor sendo necessario criar um construtor vazio
+	private final JdbcTarefaDao dao;
+	
+	@Autowired
+	public TarefasController(JdbcTarefaDao dao){
+		this.dao = dao;	
+	}
+	
+//	public TarefasController(){
+//		this(null);
+//	}
+	
 	@RequestMapping("adicionaTarefa")
 	public String adiciona(@Valid Tarefa tarefa, BindingResult result) //@Valida Verifica se tem erros no atributo descricao e se tiver volta para o formulario
 	{
@@ -26,23 +39,19 @@ public class TarefasController {
 		if (result.hasFieldErrors("descricao")) {
 			return "tarefa/formulario";
 		}
-
-		JdbcTarefaDao dao = new JdbcTarefaDao();
-		dao.adiciona(tarefa);
+	    dao.adiciona(tarefa);
 		return "tarefa/adicionada";
 	}
 
 	@RequestMapping("listaTarefas")
 	public String lista(Model model) // Possui a request (Model model) recurso do Spring o model é um objeto
 	{
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		model.addAttribute("tarefas", dao.lista());
 		return "tarefa/lista";
 	}
 
 	@RequestMapping("removeTarefa")
 	public String remove(Tarefa tarefa) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.remove(tarefa);
 		return "redirect:listaTarefas";//o "Redirect" Redireciona para o metodo listaTarefas: "@RequestMapping("listaTarefas")"
 
@@ -50,7 +59,6 @@ public class TarefasController {
 
 	@RequestMapping("mostraTarefa")
 	public String mostra(Long id, Model model) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		model.addAttribute("tarefa", dao.buscaPorId(id));
 		return "tarefa/mostra";
 
@@ -58,7 +66,6 @@ public class TarefasController {
 
 	@RequestMapping("alteraTarefa")
 	public String altera(Tarefa tarefa) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.altera(tarefa);
 		return "redirect:listaTarefas";
 
@@ -67,7 +74,6 @@ public class TarefasController {
 	@ResponseBody //Responde o status do protocolo HTTP: 200 / 404 / 500
 	@RequestMapping("finalizaTarefa")
 	public void finaliza(Long id) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.finaliza(id);
 	}
 	
